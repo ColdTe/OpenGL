750e3076239d8e6c113b5daaf2ad2ee127ad1b6f
Aditya <adityakumardex@gmail.com>
2025-12-25 09:44:37 +0530

    draw a circle

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<cmath>
+#include<vector>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
@@ -26,13 +29,89 @@ int main() {
 		glfwTerminate();
 	}
 	glViewport(0, 0, 800, 600);
+	const char* vertexShaderSource = 
+	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main(){\n"
+	"gl_Position = vec4(aPos,1.0f);\n"
+	"}";
+	const char* fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main(){\n"
+		"FragColor = vec4(0.0f,0.0f,0.0f,1.0f);\n"
+		"}";
+	int success;
+	char infoLog[512];
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "VERTEX::SHADER::COMPILATION::ERROR" << std::endl;
+		return -1;
+	}
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "FRAGMENT::SHADER::COMPILATION::ERROR" << std::endl;
+		return -1;
+	}
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "SHADER::PROGRAM::COMPILATION::ERROR" << std::endl;
+		return -1;
+	}
+	float screenHeight = 600.0f;
+	float screenWidth = 800.0f;
+	float centerX = 0.0f;
+	float centerY = 0.0f;
+	float radius = 0.5f;
+	int res = 100;
+	std::vector<float> vec;
+	vec.push_back(0.0f);
+	vec.push_back(0.0f);
+	vec.push_back(0.0f);
+	for (int i = 0; i <= res; ++i) {
+		float angle = 2.0f * 3.14159265359 * (static_cast<float>(i) / res);
+		float x = centerX + (radius * cos(angle));
+		float y = centerY + (radius * sin(angle));
+		float z = 0.0f;
+		vec.push_back(x);
+		vec.push_back(y);
+		vec.push_back(z);
+	}
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vec.size() * sizeof(float), vec.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	while (!glfwWindowShouldClose(window)) {
-		glfwSwapBuffers(window);
-		glfwPollEvents();
 		processInput(window);
 		glClearColor(0.5f, 0.1f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, res+2);  
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 	glfwTerminate();
 	return 0;
